e4ff4160c820903a21cb57253172c9e356ab6903
eddicke <adenekanademola931@gmail.com>
2017-03-10 18:49:51 -0500

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ io.on('connection', function(socket){
     console.log('i think i saw something...');
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
-    console.log('chat message', msg);
+    console.log('message: ' + msg);
   });
 });
 io.on('connection', function(socket){

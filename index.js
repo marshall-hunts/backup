835c8e395dce1d7f86aa4fc2ce673eeab76473ce
eddicke <adenekanademola931@gmail.com>
2017-03-10 18:10:42 -0500

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,12 @@ io.on('connection', function(socket){
     io.emit('chat message', msg);
   });
 });
-
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
 http.listen(port, function(){
   console.log('listening on *:' + port);
 });
